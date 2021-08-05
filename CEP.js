e946ac5d3c27329f6c5c0d1a9d47f1492ce20a5f
Hugo Barbosa da Silva <70847074+Hugo-BSilva@users.noreply.github.com>
2021-08-04 23:15:07 -0300

    Atualizando100%

--- CEP.js
+++ CEP.js
@@ -7,27 +7,21 @@ submitButton.addEventListener('click', run)
 resetButton.addEventListener('click', clear)
 
 function run (event){
-    //Previne o comportamento padrão, não deixa a página atualizar sozinha 
     event.preventDefault()
 
     /* --- VALIDANDO O CEP ---*/
     var zipCode = zipCodeField.value
 
-    /*Substitui um espaço em branco caso o usuário digite(ou ponto), por nada 
-      e remove os espaçamentos no início e no fim*/
     zipCode = zipCode.replace(' ', '')
     zipCode = zipCode.replace('.', '')
     zipCode = zipCode.trim()
 
-    //Faz a requisição para a API
     axios.get('https://viacep.com.br/ws/'+ zipCode + '/json/')
     .then(function (response){
-        //Se existir a propriedade error
         if (response.data.error) {
             throw new Error('CEP inválido')
         }
 
-        //Limpa o conteúdo, antes de preencher
         content.innerHTML = ''
         createLine('Logradouro: '+ response.data.logradouro)
         createLine('Bairro: ' + response.data.bairro)
@@ -42,7 +36,6 @@ function run (event){
 }
 
 function createLine(text){
-    //Mostra para o usuário o nome da rua segundo o CEP
     var line = document.createElement('p')
     var text = document.createTextNode(text)
     line.appendChild(text)
